c1eab5f6ee3b5a13d71ae3fc62181907731df3d9
thaomaido <122891785+thaomaido@users.noreply.github.com>
2023-01-24 15:22:00 -0500

    alert created

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// Initail Setup
+let a = 3;
+let b = 5;
+let c;
+
+let output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '--------\n';
+
+output += 'a+b = ' + (a+b)+"\n";
+output += 'a-b = ' + (a-b)+"\n";
+output += 'a*b = ' + (a*b)+"\n";
+output += 'a/b = ' + (a/b)+"\n";
+output += 'a%b = ' + (a%b)+"\n";
+output += '(a+=b) = ' + (a+=b)+"\n";
+output += '(a-=b) = ' + (a-=b)+"\n";
+output += '(a*=b) = ' + (a*=b)+"\n";
+output += '(a/=b) = ' + (a/=b)+"\n";
+output += '(a%=b) = ' + (a%=b)+"\n";
+output += '(a==b) = ' + (a==b)+"\n";
+output += '(a!=b) = ' + (a!=b)+"\n";
+output += '(a>b) = ' + (a>b)+"\n";
+output += '(a<b) = ' + (a<b)+"\n";
+output += '(!a&&!c) = ' + (!a&&!c)+"\n";
+output += '(!a||!c) = ' + (!a||!c)+"\n";
+
+alert(output);
